2d575a640b2e2618ae84f067f27ceb65c890465b
AbaevTM <abaevtm@gmail.com>
2021-02-09 19:13:52 +0300

    longest common prefix with vertical scan

---
+++ src/main/java/by/tsimur_abayeu/tasks/easy/LongestCommonPrefix.java
@@ -0,0 +1,42 @@
+package by.tsimur_abayeu.tasks.easy;
+
+import by.tsimur_abayeu.tasks.Task;
+
+import java.util.List;
+
+
+/**
+ * https://leetcode.com/problems/longest-common-prefix
+ * <p>
+ * Time complexity : O(n*m) where n - number of strings; m - size of minimal prefix Space complexity : O(1)
+ * <p>
+ * Vertical scan. Other approaches look more complex and some of them has more not needed comparisons.
+ * <p>
+ * But if you need it to be executed multiple times there's nice idea about using trie : at the end of :
+ * https://leetcode.com/problems/longest-common-prefix/solution/
+ */
+public class LongestCommonPrefix implements Task {
+
+    @Override
+    public Object solve(Object... args) {
+        return longestCommonPrefix(((List<String>) args[0]).toArray(new String[0]));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        if (strs != null && strs.length != 0) {
+            char currentChar;
+            int i;
+            for (i = 0; i < strs[0].length(); i++) {
+                currentChar = strs[0].charAt(i);
+                for (int j = 1; j < strs.length; j++) {
+                    if (i == strs[j].length() || currentChar != strs[j].charAt(i)) {
+                        return result = strs[j].substring(0, i);
+                    }
+                }
+            }
+            return result = strs[0].substring(0, i);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/by/tsimur_abayeu/tasks/easy/LongestCommonPrefixTest.java
@@ -0,0 +1,56 @@
+package by.tsimur_abayeu.tasks.easy;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.List.of;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class LongestCommonPrefixTest {
+
+    private LongestCommonPrefix task = new LongestCommonPrefix();
+
+    @Test
+    public void test_provided_1() {
+        assertThat(
+                task.solve(of("flower", "flow", "flight")),
+                is("fl")
+        );
+    }
+
+    @Test
+    public void test_provided_2() {
+        assertThat(
+                task.solve(of("dog", "racecar", "car")),
+                is("")
+        );
+    }
+
+    @Test
+    public void test_custom_1() {
+        assertThat(
+                task.solve(of("a")),
+                is("a")
+        );
+    }
+
+    @Test
+    public void test_custom_2() {
+        assertThat(
+                task.solve(of("a", "ab")),
+                is("a")
+        );
+    }
+
+    @Test
+    public void test_custom_3() {
+        assertThat(
+                task.solve(of("", "")),
+                is("")
+        );
+    }
+}
